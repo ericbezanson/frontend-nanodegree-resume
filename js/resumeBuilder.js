904c07472a8f361bfc6d05731cc99834c087c905
ericbezanson <eric.bezanson@outlook.com>
2015-07-09 02:10:52 -0300

    fixed broken bio object, used mikes genius code to create a for loop for both contact info and skills in the header

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -6,26 +6,41 @@ var bio = {
 		"email": "eric.bezanson@outlook.com",
 		"mobile": "902-403-6595",
 		"github": "https://github.com/ericbezanson",
-		"twitter": "https://twitter.com/EricBezanson",
 		"location": "Halifax, NS"
 	},
-	"picture-URL": "images/fry.jpg",
-	"welcomeMessage": "im new, its my first day",
-	"skills": "nunchuck skills, bow hunting skills, computer hacking skills"
+	"bioPic": "images/fry.jpg",
+	"welcomeMessage": "Now this is the law of the jungle, as old and as true as the sky, And the wolf that shall keep it may prosper, but the wolf that shall break it must die. As the creeper that girdles the tree trunk, the law runneth forward and back; For the strength of the pack is the wolf, and the strength of the wolf is the pack.",
+	"skills": ["nunchuck skills", "bow hunting skills", "computer hacking skills"]
 
 };
 
 var formattedName = HTMLheaderName.replace("%data%", bio.name);
 var formattedRole = HTMLheaderRole.replace("%data%", bio.role);
-var formattedContacts = HTMLcontactGeneric.replace("%data%", bio.contactInfo);
-var formattedMobile = HTMLmobile.replace("%data%", bio.contactInfo.mobile);
-var formattedWelcome = HTMLwelcomeMsg.replace("%data%", bio.welcomeMessage);
+var formattedBioPic = HTMLbioPic.replace("%data%", bio.bioPic);
+var formattedWelcomeMsg = HTMLwelcomeMsg.replace("%data%", bio.welcomeMessage);
+
+var formattedContacts = [];
+for (var key in bio.contactInfo) {
+	var html = HTMLcontactGeneric.replace('%contact%', key).replace('%data%', bio.contactInfo[key]);
+  	formattedContacts.push(html);
+}
+
+var formattedSkills = [];
+for (var key in bio.skills) {
+	var html = HTMLskills.replace("%data%",bio.skills[key]);
+	formattedSkills.push(html);
+}
+
 
 $("#header").prepend(formattedRole);
 $("#header").prepend(formattedName);
-$("#header").append(formattedContacts);
-$("#header").append(formattedmobile);
-$("#header").append(formattedWelcome);
+$("#header").append(formattedBioPic);
+$("#header").append(formattedWelcomeMsg);
+$("#header").append(HTMLskillsStart);
+$("#header").append(formattedSkills);
+$("#topContacts").append(formattedContacts.join(' '));
+
+console.log("the cake was a lie");
 
 
 
